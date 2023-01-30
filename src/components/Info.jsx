d961473c72934705c8b4ef257456dd843a7d8051
rmassieug <rodmassieu97@icloud.com>
2023-01-30 00:25:27 -0600

    Formatted components, added meta tags

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const Info = ({name, description, photo}) => {
+const Info = ({ name, description, photo }) => {
   return (
-    <section className='info'>
-        <img src={photo} alt="profile picture"/> 
-        <h1>{name}</h1>
-        <p>{description}</p>
+    <section className="info">
+      <img src={photo} alt="profile picture" />
+      <h1>{name}</h1>
+      <p>{description}</p>
     </section>
-  )
-}
+  );
+};
 
-export default Info
+export default Info;
--- src/components/LinkList.jsx
+++ src/components/LinkList.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const LinkList = ( {name, links} ) => {
-    
+const LinkList = ({ name, links }) => {
   return (
     <section className="personal">
-            <h2>{name}</h2>
-            <ul className='links'>
-                {links.map(link => (
-                    <li key={link.fields.name} className="icon-wrapper"><img className="icon" src={link.fields.icon.fields.file.url} alt={`${link.fields.name}-icon`}/><a href={link.fields.url} target='_blank'><span>{link.fields.name}</span></a></li>
-                ))}
-            </ul>
-        </section>
-  )
-}
+      <h2>{name}</h2>
+      <ul className="links">
+        {links.map((link) => (
+          <li key={link.fields.name} className="icon-wrapper">
+            <img
+              className="icon"
+              src={link.fields.icon.fields.file.url}
+              alt={`${link.fields.name}-icon`}
+            />
+            <a href={link.fields.url} target="_blank">
+              <span>{link.fields.name}</span>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
 
-export default LinkList
+export default LinkList;
