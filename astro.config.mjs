39da5bed5a4f168653a86665eb086194eb5ecab3
rmassieug <rodmassieu97@icloud.com>
2023-01-30 12:57:02 -0600

    Injected GoogleAnalytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
-    integrations: [react()],
+  integrations: [react(), partytown()]
 });
